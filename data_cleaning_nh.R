822d017c6c49121dc6345b9d6e7b57ff1c6236d7
Nicholas Harp <nharp@huskers.unl.edu>
2019-08-19 12:23:22 -0500

    move RT prelim analyses to separate script
    need to keep scripts clean if we'll be sourcing them for the manuscript. removed the RT stuff that I'd put in for determining our cutoff

--- data_cleaning_nh.R
+++ data_cleaning_nh.R
@@ -1,7 +1,7 @@
 ### set wd ###
 cbpath <- '~/Documents/GitHub/Words/'
 nhpath <- '~/Documents/Nick-Grad/Neta_Lab/Words/'
-path <- cbpath
+path <- nhpath
 setwd(path)
 
 ### load v important packages, but quietly ###
@@ -115,27 +115,6 @@ words.summary <- (ddply(data, "wordlist", plyr::summarise,
 
 # write.csv(words.summary, "~/Documents/Nick-Grad/Neta_Lab/Words/words.summary.csv")
 write.csv(words.summary,paste(path,"words.summary",'.csv',sep = ''))
-
-### plot all RTs per subj ###
-data$`Participant Public ID` <- as.character(data$`Participant Public ID`)
-ggplot(data = data, aes(x = `Participant Public ID`, y = `Reaction Time`)) +
-  geom_point() +
-  ylim(0, 10000) +
-  geom_hline(mapping = NULL, yintercept = 2932.776)
-
-### calculate percent trials retained after RT cutoff ###
-orig <- data %>% count(`Participant Public ID`)
-sub <- subset(data, (`Reaction Time` >= 250 & `Reaction Time` <= 2214.349)) %>% count(`Participant Public ID`)
-comb <- merge(orig, sub, by = "Participant Public ID")
-comb$PercentRemaining <- (comb$n.y / comb$n.x)
- write.csv(comb, "~/Desktop/250to2214ms.csv")
-
-
-data2 <- subset(data, (`Reaction Time` >= 250 & `Reaction Time` <= 3000))
-
-words.summary <- words.summary[order(words.summary$neg.avg),]
-
-row.names(words.summary) <- NULL
 
 # list <- count(data2$`Participant Public ID`)
 # count(list$freq < (627/2))
